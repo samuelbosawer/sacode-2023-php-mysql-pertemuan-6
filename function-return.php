33c7601d0f33c473c3142379c5df2645ea313ff4
Samuel Bosawer <zam.bosawer@gmail.com>
2023-09-26 20:13:56 +0900

    Function Return

---
+++ function-return.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+function hitung(int $a, int $b)
+{
+    $c = $a + $b ;
+  return $c  ;
+}
+
+echo hitung(12,7);
---
+++ function-type-data.php
@@ -0,0 +1,9 @@
+<?php
+
+function angka(int $a, int $b)
+{
+    $c = $a + $b; 
+    echo $c;
+}
+
+angka(5,5);
